c7125d2fbc7a81f9303dab0e724731a2ca143fe0
hongxia <xiahahaha01@gmail.com>
2022-08-08 19:01:48 +0800

    feat:add error-bp neuron-synpase

--- dagi/cogi_arch.py
+++ dagi/cogi_arch.py
@@ -76,15 +76,37 @@ class HumanBrainAgent(Agent):
     """基于对大脑智能仿真的智能体，关注内部模块
     核心概念：动力学；目标系统；决策规划；短期记忆；注意；学习；图像；自然语言；
     TODO : 考虑一些非计算的特性，比如注意力（能量的分配），大脑疲劳机制，多巴胺失调，做梦
+
+
+    ### 学习
+    学习，我认为一定存在某种目标。对于模块或者每个突触都是这样。
+    我希望在接口层面，可以定义目标；实现层面，保证目标的学习通过局部动力学实现。
     """
 
     def build_network():
-        network = Network(activation = tf.relu, leak = 0.1)
+        synpase_type = type("BaseSynpase", (LinearSynpase, ErrorBPSynpase))
+        network = Network(activation = tf.relu, leak = 0.1, error = True, synpase_type = synpase_type)
 
         # reward
         target = Neurons(1)
         sub_targets = Neurons(1000, self_dynamics=rnn_train)
         network.link(sub_targets, target, "mlp:2", layer_sizes = [] )
+        # outer reward
+        input_reward = Neunrons(1)
+        # 应该认识到强化学习中，行为与回报不存在显式的计算图。
+        # td_learning 根据预估与实际的error来学习minize(|v(s')+r-q(s,get_action(s))|)
+        # TODO: implament AC error
+        # Q值，多巴胺
+        # 方法1：简单维护一个q(s,a)函数
+        # 方法2: 保存一个s'=P(s,a)的副本呢？然后再创建一个target的副本
+        # 方法3：不创建副本，而是通过时序，通过想象s'，自然生成Q值
+        future_target = Neurons(1)
+        network.link([state, actions],future_target, "mlp:3")
+        last_future_target = Neurons(1)
+        network.link(future_target, last_future_target, "mlp:1")
+        # 注意到上一次的future_target与target的差形成训练信号
+        msesynpase([last_future_target,target+input_reward])
+        # 把这里的机制封装起来
 
         # lang-working-memroy
         lang_state = Neurons(1000000, self_dynamics=rnn_train)
@@ -104,6 +126,7 @@ class HumanBrainAgent(Agent):
         def CNN(states, weights):
             return model(states)
         network.link(input_vision, vision_state, CNN)
+        # learning: pc
         
         input_phone = Neurons(1000, leak_init=1)
         input_char = Neurons(1000, leak_init=1)
@@ -111,20 +134,23 @@ class HumanBrainAgent(Agent):
         def RNN(states, weights):
             return model(states)
         network.link(input_phone, lang_state, RNN)
+        # learning: pc
 
         # top-down attention
         
         # attention, conscioueness, energy
 
+        # Module: Decision
         # policy-based decision
         actions = Neurons(1000,leak_init = 1)
         network.link(state, actions, "mlp:3")
-        # forward could be either graph or dynamics based. equally.
+
+        # Planning
         # Q(V(s,a)) Q(V(s,[a1,a2,..])) 都涉及到了时序过程，短期记忆的参与，而不是独立的结构
         # 大脑是否有某个东西来指导包括这个思考过程呢？？即短期记忆的思考过程
         # 换言之. planning是被转化为RNN参数而非显式编码的
-        
-        # 决策结果应该可以影响思考过程
+
+        # 决策结果（包括置信度) 应该可以影响思考过程
         uncerntaty = F(actions)
         network.link(actions, state, "mlp:3")
         network.link(sub_targets, state, "mlp:3")
--- dynamic_net/predictive_coding.py
+++ dynamic_net/predictive_coding.py
@@ -21,46 +21,34 @@ Song, Yuhang, Thomas Lukasiewicz, Zhenghua Xu, and Rafal Bogacz. 2020. ‘Can th
 Lillicrap, Timothy P., Adam Santoro, Luke Marris, Colin J. Akerman, and Geoffrey Hinton. 2020. ‘Backpropagation and the Brain’. Nature Reviews Neuroscience 21 (6): 335–46. https://doi.org/10.1038/s41583-020-0277-3.
 
 
+### error收敛
+给每个神经元配一个error，设计动力学，当收敛时error的形式恰好符合error传播。
+有几个工作：
+* 通过error来传播，Backpropagation and the Brain, 实现为mlp_predicitive
+* 不影响前馈，error按反向传播变成对应的动力学，实现为mlp_bp_predicitive
+* 关于反馈网络的近似bp算法(需要解决bptt的难点)
+* target产生: 对未来的预测产生mse-target
 """
 
 import argparse
 import os
 import sys
 
-"""
-### error收敛
-给每个神经元配一个error，设计动力学，当收敛时error的形式恰好符合error传播。
-error传播即反向传播，所以就是在原有网络连接上，每个连接配的；y=f(z=Wx1+Wx2+...)
-dC/dx1 = dC/dy*dy/dx1 = error_y*f'(z)*W error_x=W^T*error_y
-* 原网路可以保持不变；
-* 也可以受error影响转到新状态
-* 最后的学习依赖error与原状态(或者新状态)
-
-整体的过程是：
-* 预测，产生error
-* error传播，训练
-
-以下分别实现
-* 基于多层MLP
-* 基于一般计算图的，包括CNN, RNN
-* 基于反馈神经网络
-* 基于一般动力学神经网络
-"""
-
-def error_neuron_fn(object):
-    states1, states2 = states
-    error = states2 - weights*states1
-    update1 = tf.transpose(weights)*error
-    update2 = -error
-    return [update1, update2]
-
-def main(
+def mlp_predicitive(
+    # 标准的predicitive-coding mlp
+    # 通过error来进行上下层的沟通，具体参考Backpropagation and the Brain
         layer_num,
         neurons,
         synpases,
         input_inxs,
         output_inxs,
         stop_th=0.1):
+    def error_neuron_fn(states):
+        states1, states2 = states
+        error = states2 - weights*states1
+        update1 = tf.transpose(weights)*error
+        update2 = -error
+        return [update1, update2]
     x_neurons = []
     x_neurons.append(Neusons())
     synpases = []
@@ -69,8 +57,44 @@ def main(
         synpase = BiLinearSynpase("synpase"+str(l),x_neurons[l-1], x_neurons[l],
                                  error_neuron_fn, hebbian_synpase_fn)
         synpase.append(synpase)
-
     network = Network(x_neurons, synpases)
+
+def mlp_bp_predicitive(
+        layer_num,
+        neurons,
+        synpases,
+        input_inxs,
+        output_inxs,
+        stop_th=0.1):
+    """ 一种与bp更相似的网络。每个神经元包含(states, error)，前向不变，后向每个神经元配个error按bp方式进行传播。
+    只有当target真正触发了，非0-error才会传递到整个网络。
+    注意到这里一定得是前向网络，并且输入保持一段时间的稳定才行。"""
+    x_neurons = []
+    x_neurons.append(Neusons(error=True))
+    synpases = []
+    PSynpase = type("PSynpase",(LinearSynpase, ErrorBPSynpase))
+    for l in range(1, layer_num):
+        x_neurons.append(Neurons(error=True))
+        synpase = PSynpase("synpase"+str(l),x_neurons[l-1], x_neurons[l])
+        synpase.append(synpase)
+    target = ForwardNeurons(error=True)
+    y_true = Neurons(error=True)
+    # error项是target对神经元的微分。target可以是分布式多个来源的。
+    # 我们只要描述目标神经元与目标的关系,然后采取TargetBPSynpase，把target-states转化为error
+    Tsynpase = type("TSynpase",(TargetBPSynpase, MseSynpase))
+    synpase = TSynpase("tsynpase", [x_neurons[layer_num-1], y_true, target])
+    synpase.append(synpase)
+    x_neurons+=[y_true, target]
+    network = Network(x_neurons, synpases)
+
+# 反馈神经网络存在困难，需要特殊处理：
+# * 或者使用短期记忆保存历史状态，然后使用BP
+# * 或者使用前向梯度，复杂度太高，需要考虑近似策略
+# 思考记在知识库，TODO
+
+# local_predicitve_coding  局部可以产生自我预测的error
+# 可以是空间或者时间的
+# 时间涉及与到预测时间窗口的问题。TODO
 
 
 if __name__ == '__main__':
